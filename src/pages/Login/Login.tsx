cb0e30781a7221e4a90f11d09929dd7933f39395
wuzoo <wndyd9911@gmail.com>
2024-01-18 21:43:37 +0900

    feat: Login 페이지 컴포넌트 구성 완료

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,4 +1,30 @@
+import * as Styled from "./style";
+import Header from "../../components/Header/Header";
+import img from "../../assets/3dicons/login/person.svg";
+import Typo from "../../components/Typo/Typo";
+
 function Login() {
-  return <h1>Login</h1>;
+  return (
+    <>
+      <img css={Styled.pagelogo} src={img} />
+      <Header type="login" />
+      <div css={Styled.page}>
+        <div css={Styled.loginbox}>
+          <h1 css={Styled.welcometext}>
+            LIKELION <br />
+            INHA UNIV 12기와 <br />
+            함께하기
+          </h1>
+          <Styled.Input placeholder="아이디를 입력하세요" />
+          <Styled.Input placeholder="비밀번호를 입력하세요" />
+          <Styled.loginbtn>
+            <Typo fontSize="20" weight="600" color="white">
+              로그인
+            </Typo>
+          </Styled.loginbtn>
+        </div>
+      </div>
+    </>
+  );
 }
 export default Login;
