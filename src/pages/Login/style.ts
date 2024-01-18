58ce6a6d2999cd956a7c598641f98dad85170b66
wuzoo <wndyd9911@gmail.com>
2024-01-18 21:43:54 +0900

    style: Login 페이지 컴포넌트 스타일 작성

---
+++ src/pages/Login/style.ts
@@ -0,0 +1,55 @@
+import { css } from "@emotion/react";
+import styled from "@emotion/styled";
+
+export const page = css`
+  width: 100vw;
+  display: flex;
+  justify-content: center;
+`;
+
+export const loginbox = css`
+  max-width: 60vw;
+  max-height: 80vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 15rem;
+`;
+
+export const welcometext = css`
+  font-size: 32px;
+  font-weight: 600;
+  text-align: right;
+  place-self: end;
+  line-height: 1.2;
+`;
+
+export const Input = styled.input`
+  border: 2px solid ${(props) => props.theme.color.lightgray};
+  width: 30vw;
+  height: 50px;
+  margin: 5px 0px;
+  border-radius: 10px;
+
+  &::placeholder {
+    padding-left: 24px;
+    font-size: 18px;
+  }
+`;
+
+export const pagelogo = css`
+  position: absolute;
+  width: 400px;
+  left: 20%;
+  top: 15%;
+  z-index: -1000;
+`;
+
+export const loginbtn = styled.button`
+  width: 70%;
+  border: none;
+  background-color: ${(props) => props.theme.color.darkblue};
+  margin-top: 2rem;
+  height: 40px;
+  border-radius: 10px;
+`;
