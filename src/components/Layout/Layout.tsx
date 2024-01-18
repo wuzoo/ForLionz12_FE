c0ead0326f2fd5157e427d6399f1cf4a7f9a32b1
wuzoo <wndyd9911@gmail.com>
2024-01-18 20:02:01 +0900

    feat: Header 추가

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,9 +1,10 @@
 import { Outlet } from "react-router-dom";
+import Header from "../Header/Header";
 
 function Layout() {
   return (
     <>
-      <h1>navigation</h1>
+      <Header />
       <Outlet />
     </>
   );
