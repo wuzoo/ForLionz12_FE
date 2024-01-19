8581b2f6fb45868ed96875c3e0ef151cbcc5f815
wuzoo <wndyd9911@gmail.com>
2024-01-19 20:52:38 +0900

    style: Button 컴포넌트에서 css prop으로 받음으로써 일부 스타일 제거

--- src/components/Button/style.ts
+++ src/components/Button/style.ts
@@ -1,11 +1,5 @@
 import styled from "@emotion/styled";
-import { IBtnStyle } from "./types";
 
-export const Btn = styled.button<IBtnStyle>`
-  width: ${(props) => props.width};
+export const Btn = styled.button`
   border: none;
-  background-color: ${(props) =>
-    props.color ? props.theme.color[props.color] : props.theme.color.darkblue};
-  height: ${(props) => props.height || "30"}px;
-  border-radius: ${(props) => props.radius || "10"}px;
 `;
