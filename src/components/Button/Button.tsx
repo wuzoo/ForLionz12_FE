4a0fbf310ab761067769de53b024bc39c5d61889
wuzoo <wndyd9911@gmail.com>
2024-01-19 20:51:56 +0900

    refactor: Button css prop으로 수정

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,9 +1,22 @@
+import { css } from "@emotion/react";
 import * as Styled from "./style";
 import { IBtn } from "./types";
+import { theme } from "../../theme/theme";
 
 function Button(props: IBtn) {
   return (
-    <Styled.Btn {...props} onClick={props.onClick}>
+    <Styled.Btn
+      {...props}
+      onClick={props.onClick}
+      css={css`
+        width: ${props.width};
+        background-color: ${props.color
+          ? theme.color[props.color]
+          : theme.color.darkblue};
+        border-radius: ${props.radius || "10"}px;
+        height: ${props.height || "30"}px;
+      `}
+    >
       {props.children}
     </Styled.Btn>
   );
