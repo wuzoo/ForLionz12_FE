0d18919a4ff7f4d06588214d42b4449d207d9606
wuzoo <wndyd9911@gmail.com>
2024-01-19 16:49:34 +0900

    style: css를 styled로 변경, loginbox에서 btn 위의 마진 부여, focus시 input outline: none

--- src/pages/Login/style.ts
+++ src/pages/Login/style.ts
@@ -1,51 +1,48 @@
-import { css } from "@emotion/react";
 import styled from "@emotion/styled";
 
-export const page = css`
+export const page = styled.div`
   width: 100vw;
   display: flex;
   justify-content: center;
 `;
 
-export const loginbox = css`
+export const loginbox = styled.div`
   max-width: 60vw;
   max-height: 80vh;
   display: flex;
   flex-direction: column;
   align-items: center;
   margin-top: 15rem;
+
+  button {
+    margin-top: 25px;
+  }
 `;
 
-export const welcometext = css`
+export const welcometext = styled.h1`
   font-size: 32px;
   font-weight: 600;
   text-align: right;
+  white-space: pre-line;
   place-self: end;
   line-height: 1.2;
 `;
 
 export const Input = styled.input`
-  border: 2px solid ${(props) => props.theme.color.lightgray};
+  border: 2.5px solid ${(props) => props.theme.color.lightgray};
   width: 30vw;
   height: 50px;
   margin: 5px 0px;
   border-radius: 10px;
+  font-size: 18px;
+  padding-left: 26px;
 
   &::placeholder {
-    padding-left: 24px;
     font-size: 18px;
   }
 
   &:focus {
     border-color: ${(props) => props.theme.color.darkblue};
+    outline: none;
   }
-`;
-
-export const loginbtn = styled.button`
-  width: 70%;
-  border: none;
-  background-color: ${(props) => props.theme.color.darkblue};
-  margin-top: 2rem;
-  height: 40px;
-  border-radius: 10px;
 `;
