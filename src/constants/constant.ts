e6d620a26243ff5eb6e0f246613975f4a2f43ef8
wuzoo <wndyd9911@gmail.com>
2024-01-19 17:14:34 +0900

    feat: 페이지의 정적 text들을 위한 constants 파일 생성

---
+++ src/constants/constant.ts
@@ -0,0 +1,26 @@
+export const text = {
+  login: "LIKELION\nINHA UNIV 12기와\n함께하기",
+  main: "Growl\nTo Growth\nWelcome",
+  greeting: "님 반갑습니다",
+  contact: "사자들의 정보를 볼 수 있어요.",
+  notification: "어떤 일이 기다리고 있을까요 ?",
+  assignlist: {
+    main: "나의 과제 목록을 확인하세요",
+    otherpart: "다른 파트의 과제들을 구경할 수 있어요.",
+  },
+  assignsubmit: {
+    main: "최근 업로드된 다른 아기사자의 과제와 지금까지 업로드된 과제들을 구경할 수 있어요.",
+    recentupload: "최근 업로드된 아기사자의 과제를 구경하세요 !",
+    explain: "짧게 적어도 됩니다. 자신의 과제를 마음껏 표현해주세요.",
+    link: "과제 깃허브 링크 또는 배포 링크",
+    other: "아기사자들의 과제들을 구경해요.",
+  },
+  qna: "깨달음은 호기심으로부터.",
+  question: {
+    title: "제목을 입력해주세요.",
+    tags: "질문 내용에 해당하는 태그를 선택해주세요.",
+    code: "코드를 첨부하려면 캡쳐하여 올려주세요.",
+    content: "궁금한 점을 자세히 알려주세요",
+  },
+  profile: "사자들에게 보여질 프로필을 수정할 수 있어요.",
+};
