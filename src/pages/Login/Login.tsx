98f9f4a50d9ac05383654f28f5d15ae7dc566fab
wuzoo <wndyd9911@gmail.com>
2024-01-19 16:48:37 +0900

    refactor: Btn을 Button 컴포넌트로 변경, login text recoilvalue로 대체

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -2,28 +2,30 @@ import * as Styled from "./style";
 import Header from "../../components/Header/Header";
 import Typo from "../../components/Typo/Typo";
 import PageLogo from "../../components/PageLogo/PageLogo";
+import Button from "../../components/Button/Button";
+import { useRecoilValue } from "recoil";
+import { Text } from "../../recoil/texts";
 
 function Login() {
+  const { login } = useRecoilValue(Text);
+
   return (
     <>
       <PageLogo type="login" left="20%" top="15%" />
       <Header type="login" />
-      <div css={Styled.page}>
-        <div css={Styled.loginbox}>
-          <h1 css={Styled.welcometext}>
-            LIKELION <br />
-            INHA UNIV 12기와 <br />
-            함께하기
-          </h1>
+      <Styled.page>
+        <Styled.loginbox>
+          <Styled.welcometext>{login}</Styled.welcometext>
           <Styled.Input placeholder="아이디를 입력하세요" />
           <Styled.Input placeholder="비밀번호를 입력하세요" />
-          <Styled.loginbtn>
+
+          <Button width="60%" height="40" onClick={() => {}}>
             <Typo fontSize="20" weight="600" color="white">
               로그인
             </Typo>
-          </Styled.loginbtn>
-        </div>
-      </div>
+          </Button>
+        </Styled.loginbox>
+      </Styled.page>
     </>
   );
 }
