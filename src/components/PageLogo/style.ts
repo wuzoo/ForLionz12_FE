31fdb6476cb21613e93287b031f622c0a3d2bb62
wuzoo <wndyd9911@gmail.com>
2024-01-19 14:37:04 +0900

    style: position, height prop 추가

--- src/components/PageLogo/style.ts
+++ src/components/PageLogo/style.ts
@@ -1,10 +1,12 @@
 import styled from "@emotion/styled";
-import { IImg } from "./types";
+import { ILogo } from "./types";
 
-export const Img = styled.img<IImg>`
-  position: absolute;
+export const Img = styled.img<ILogo>`
+  position: ${(props) => props.pos || "absolute"};
   z-index: -1000;
   width: ${(props) => props.width || "400"}px;
+  height: ${(props) => props.height || "400"}px;
+
   left: ${(props) => props.left || 0};
   top: ${(props) => props.top || 0};
   right: ${(props) => props.right || 0};
