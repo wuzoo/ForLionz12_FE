53ef5dfccb00281c9bf37fa03432064d9e9570d8
wuzoo <wndyd9911@gmail.com>
2024-01-20 21:27:12 +0900

    refactor: svg PageLogo 위치 prop 수정

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -3,16 +3,16 @@ import Header from "../../components/Header/Header";
 import Typo from "../../components/Typo/Typo";
 import PageLogo from "../../components/PageLogo/PageLogo";
 import Button from "../../components/Button/Button";
-import { text } from "../../constants/constant";
+import { TEXT } from "../../constants/text";
 
 function Login() {
   return (
     <>
-      <PageLogo type="login" left="20%" top="15%" />
+      <PageLogo width="350" height="350" type="login" left="22%" top="23%" />
       <Header type="login" />
       <Styled.page>
         <Styled.loginbox>
-          <Styled.welcometext>{text.login}</Styled.welcometext>
+          <Styled.welcometext>{TEXT.login}</Styled.welcometext>
           <Styled.Input placeholder="아이디를 입력하세요" />
           <Styled.Input placeholder="비밀번호를 입력하세요" />
 
