c887d0b79130a08943a52ed05db0277f55309e21
wuzoo <wndyd9911@gmail.com>
2024-01-20 21:13:29 +0900

    feat: FullScreenSlider 카드 style + type 작성

---
+++ src/components/Card/SliderCard/style.ts
@@ -0,0 +1,42 @@
+import styled from "@emotion/styled";
+
+export const CardWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  min-height: 400px;
+  padding-bottom: 36px;
+  position: relative;
+  z-index: -10;
+`;
+
+export const Picture = styled.div`
+  width: 340px;
+  height: 340px;
+  background-size: contain;
+  background-position: center center;
+  background-repeat: no-repeat;
+  position: absolute;
+  top: 10%;
+  right: 48%;
+  z-index: -1;
+`;
+
+export const LogoAndTitle = styled.div`
+  max-width: 670px;
+  height: 300px;
+  display: flex;
+  align-items: center;
+  justify-content: end;
+  width: 500px;
+`;
+
+export const TitleWrapper = styled.div`
+  text-align: end;
+`;
+
+export const Alignwrapper = styled.div`
+  display: flex;
+  align-items: center;
+`;
---
+++ src/components/Card/SliderCard/types.ts
@@ -0,0 +1,8 @@
+export interface ICard {
+  logo: string;
+  bgcolor: string;
+  main: string;
+  sub: string;
+  part: string;
+  date: string;
+}
