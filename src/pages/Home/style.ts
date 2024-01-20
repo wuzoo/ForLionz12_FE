d69d6ebe90ed59de6ef8113221da54a88bb64290
wuzoo <wndyd9911@gmail.com>
2024-01-20 21:25:12 +0900

    style: 홈 페이지 배너, 텍스트 style 작성

---
+++ src/pages/Home/style.ts
@@ -0,0 +1,21 @@
+import styled from "@emotion/styled";
+
+export const banner = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 5rem;
+  min-height: 400px;
+`;
+
+export const textwrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  gap: 20px;
+`;
+
+export const text = styled.h1`
+  white-space: pre-line;
+`;
