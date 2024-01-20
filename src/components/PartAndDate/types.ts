cce18579960446656e96f1067cb8c7d331646452
wuzoo <wndyd9911@gmail.com>
2024-01-20 14:29:37 +0900

    feat: PartAndDate 컴포넌트 타입 export

---
+++ src/components/PartAndDate/types.ts
@@ -0,0 +1,12 @@
+export interface IPart {
+  part: string;
+  showbadge?: boolean;
+  fontSize?: string;
+}
+
+export interface IDate {
+  date: string;
+  showtext?: boolean;
+  showdelim?: boolean;
+  fontSize?: string;
+}
