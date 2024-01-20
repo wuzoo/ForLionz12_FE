a5c07f8ec011c217054d256fcff463d3d5c64dfc
wuzoo <wndyd9911@gmail.com>
2024-01-20 21:05:51 +0900

    feat: custom arrow를 위한 prev, next 화살표 컴포넌트 생성 + style

---
+++ src/components/Slider/components/style.ts
@@ -0,0 +1,23 @@
+import styled from "@emotion/styled";
+
+export const Left = styled.img`
+  width: 50px;
+  height: 50px;
+  cursor: pointer;
+  position: absolute;
+  left: 5%;
+  top: calc(50% - 26px);
+  z-index: 10;
+  opacity: 0.5;
+`;
+
+export const Right = styled.img`
+  width: 50px;
+  height: 50px;
+  position: absolute;
+  top: calc(50% - 26px);
+  right: 5%;
+  cursor: pointer;
+  z-index: 10;
+  opacity: 0.5;
+`;
