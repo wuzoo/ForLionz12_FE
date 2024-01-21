a6e7890d966b3c76dde6d30d3e0802daeec77df4
wuzoo <wndyd9911@gmail.com>
2024-01-21 16:40:05 +0900

    refactor: 모든 컨텐츠의 제목과 소제목을 그룹화한 컴포넌트, style 수정

--- src/components/MainAndSubtitle/style.ts
+++ src/components/MainAndSubtitle/style.ts
@@ -6,12 +6,4 @@ export const Title = styled.p`
 
 export const Content = styled.p`
   width: auto;
-  line-height: 1.2;
-  padding-left: 8rem;
-  overflow: hidden;
-  text-overflow: ellipsis;
-  text-decoration-color: white;
-  display: -webkit-box;
-  -webkit-line-clamp: 2;
-  -webkit-box-orient: vertical;
 `;
