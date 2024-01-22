249bdea02655147cdf84a0cba25a8cd6fadabea9
wuzoo <wndyd9911@gmail.com>
2024-01-22 17:22:07 +0900

    refactor: 커스텀 훅 내 flag를 추가하여 과제냐 공지냐에 따라 토글 state를 달리 반환

--- src/hooks/useSelectedPart.ts
+++ src/hooks/useSelectedPart.ts
@@ -1,9 +1,10 @@
 import { useState } from "react";
 
-type ReturnTypes = [string, () => void, () => void, () => void];
+type ReturnTypes = [string, () => void, () => void, () => void, boolean];
 
-function useSelectedPart(): ReturnTypes {
+function useSelectedPart(type: string): ReturnTypes {
   const [part, setPart] = useState("");
+  const [flag, setFlag] = useState(false);
 
   const feclickhandler = () => {
     console.log("fe clicked !");
@@ -13,12 +14,16 @@ function useSelectedPart(): ReturnTypes {
     console.log("be clicked !");
     setPart("be");
   };
-  const staffclickhandler = () => {
-    console.log("staff clicked !");
-    setPart("staff");
+  const otherclickhandler = () => {
+    if (type == "part") {
+      setPart("staff");
+    } else if (type == "notice") {
+      setFlag(true);
+      setPart("all");
+    }
   };
 
-  return [part, feclickhandler, beclickhandler, staffclickhandler];
+  return [part, feclickhandler, beclickhandler, otherclickhandler, flag];
 }
 
 export default useSelectedPart;
