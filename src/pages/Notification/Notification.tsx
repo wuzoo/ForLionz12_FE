071230f6f24d1ac996e653455ed43f49c6beb3c7
wuzoo <wndyd9911@gmail.com>
2024-01-22 17:25:56 +0900

    feat: 공지사항 페이지 컴포넌트 구조 설계 및 스타일 작성

--- src/pages/Notification/Notification.tsx
+++ src/pages/Notification/Notification.tsx
@@ -1,5 +1,38 @@
+import Banner from "../../components/Banner/Banner";
+import PartToggle from "../../components/PartToggle/PartToggle";
+import useSelectedPart from "../../hooks/useSelectedPart";
+import ListItem from "./components/ListItem";
+import * as Styled from "./style";
+
 function Notification() {
-  return <h1>notification</h1>;
+  const [part, feclick, beclick, allclick] = useSelectedPart("notice");
+
+  return (
+    <Styled.Wrapper>
+      <Banner type="notification" logowidth="500" logoheight="500" />
+      <Styled.Toggle>
+        <PartToggle
+          part={part}
+          showfe={feclick}
+          showbe={beclick}
+          showother={allclick}
+          flag={true}
+        />
+      </Styled.Toggle>
+      <Styled.Items>
+        <ListItem
+          part="fe"
+          content="멋쟁이사자처럼은 지금까지의 일정을 모두 뒤로하고 폐지하기로 결정하였습니다. 죄송합니다."
+          date="2023-04-10 19:00:00"
+        />
+        <ListItem
+          part="be"
+          content="저 Spring 모릅니다.. 저한테 물어보시면 큰일나요."
+          date="2023-04-10 19:00:00"
+        />
+      </Styled.Items>
+    </Styled.Wrapper>
+  );
 }
 
 export default Notification;
---
+++ src/pages/Notification/style.ts
@@ -0,0 +1,20 @@
+import styled from "@emotion/styled";
+
+export const Wrapper = styled.div`
+  width: 70%;
+`;
+
+export const Toggle = styled.div`
+  width: 95%;
+  display: flex;
+  justify-content: flex-end;
+  margin-bottom: 20px;
+`;
+
+export const Items = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: start;
+  width: 95%;
+  gap: 12px;
+`;
