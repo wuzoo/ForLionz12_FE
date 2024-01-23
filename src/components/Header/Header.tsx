d734370f0869cd0392e4afe29a7cc4d1f0311609
wuzoo <wndyd9911@gmail.com>
2024-01-23 23:35:52 +0900

    feat: profile 클릭 시 마이페이지 리다이렉션 + indicator bar 컴포넌트 추가

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,13 +1,20 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import { IHeader } from "./types";
 import * as Styled from "./style";
 import Profile from "../Profile/Profile";
 import tmp from "../../assets/imgs/tmpprofile.jpeg";
 import Typo from "../Typo/Typo";
+import MotionBar from "./component/MotionBar";
 
 function Header({ type }: IHeader) {
   const isLoginPage = Boolean(type);
+  const navigate = useNavigate();
   // const isAuthenticated = useAuth();
+  const { pathname } = useLocation();
+
+  const 마이페이지가기 = () => {
+    navigate("/profile");
+  };
 
   return (
     <Styled.Wrapper type={type}>
@@ -18,21 +25,36 @@ function Header({ type }: IHeader) {
         {!isLoginPage && (
           <>
             <Link to="/notification">
-              <Typo weight="600">Notification</Typo>
+              <Typo weight="600">
+                Notification
+                {pathname == "/notification" && <MotionBar />}
+              </Typo>
             </Link>
             <Link to="/homework-list">
-              <Typo weight="600">Assignment</Typo>
+              <Typo weight="600">
+                Assignment
+                {pathname == "/homework-list" && <MotionBar />}
+              </Typo>
             </Link>
             <Link to="/qna">
-              <Typo weight="600">Q&A</Typo>
+              <Typo weight="600">
+                Q&A
+                {pathname == "/qna" && <MotionBar />}
+              </Typo>
             </Link>
             <Link to="/contact">
-              <Typo weight="600">Contact</Typo>
+              <Typo weight="600">
+                Contact
+                {pathname == "/contact" && <MotionBar />}
+              </Typo>
             </Link>
           </>
         )}
       </Styled.navcol>
-      <Styled.headerprofile show={!isLoginPage ? true : false}>
+      <Styled.headerprofile
+        onClick={마이페이지가기}
+        show={!isLoginPage ? true : false}
+      >
         <Profile url={tmp} size="50" />
       </Styled.headerprofile>
     </Styled.Wrapper>
