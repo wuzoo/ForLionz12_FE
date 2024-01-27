aead8b1cbf60110e8dbd58a0b1b88f2731f54d4c
wuzoo <wndyd9911@gmail.com>
2024-01-27 12:38:47 +0900

    feat: 사이드바 스크롤시 최상단에 이르면 fixed되어 고정되도록 구현

--- src/pages/Qna/components/SideBar/SideBar.tsx
+++ src/pages/Qna/components/SideBar/SideBar.tsx
@@ -1,17 +1,49 @@
 import { css } from "@emotion/react";
 import * as Styled from "./style";
+import { useEffect, useRef } from "react";
 
 interface ISideBar {
   setCategory: (e: string) => void;
 }
 
 function SideBar({ setCategory }: ISideBar) {
+  const ref = useRef<HTMLDivElement>(null);
+
+  const SidebarControl = () => {
+    console.log(window.innerHeight);
+    console.log(document.documentElement.clientHeight);
+
+    const origin_y = ref.current?.getBoundingClientRect().top;
+
+    const pos_y = window.scrollY;
+    const obj = ref.current?.style;
+
+    if (!obj) return;
+    if (!origin_y) return;
+
+    if (pos_y > (window.innerHeight * 7) / 10) {
+      ref.current.style.position = "fixed";
+      ref.current.style.top = "80px";
+    } else {
+      ref.current.style.position = "absolute";
+      ref.current.style.left = "0";
+      ref.current.style.top = "70vh";
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", SidebarControl);
+
+    return () => window.removeEventListener("scroll", SidebarControl);
+  }, []);
+
   return (
     <div
+      ref={ref}
       css={css`
         position: absolute;
-        left: 0px;
-        width: 15%;
+        left: 0;
+        width: 10%;
         max-width: 180px;
       `}
     >
