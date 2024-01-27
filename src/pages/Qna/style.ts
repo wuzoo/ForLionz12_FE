0bb26b16895a449449a2ed9b8e294f458fd50b68
wuzoo <wndyd9911@gmail.com>
2024-01-27 12:44:49 +0900

    style: 큐앤에이 페이지 스타일 작성

---
+++ src/pages/Qna/style.ts
@@ -0,0 +1,20 @@
+import styled from "@emotion/styled";
+
+export const Wrapper = styled.div`
+  width: 70%;
+  min-height: 150vh;
+`;
+
+export const Title = styled.h1``;
+
+export const BoxContainer = styled.div`
+  display: flex;
+  gap: 10px;
+  margin: 20px 0px;
+`;
+
+export const ItemsContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 15px;
+`;
