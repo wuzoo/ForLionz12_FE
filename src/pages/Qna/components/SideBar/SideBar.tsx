01615130a893a94e8c04d29fb6cb6b941a271992
wuzoo <wndyd9911@gmail.com>
2024-01-27 20:59:49 +0900

    feat: CATEGORY_TEXT 객체 선언으로 entries로 item 표시하기 + currentCategory state를 prop으로 받아와 현재 선택된 item 표시

--- src/pages/Qna/components/SideBar/SideBar.tsx
+++ src/pages/Qna/components/SideBar/SideBar.tsx
@@ -1,60 +1,28 @@
 import { css } from "@emotion/react";
 import * as Styled from "./style";
-import { useEffect, useRef } from "react";
+import { CATEGORY_TEXT } from "../constants/text";
+import { theme } from "../../../../theme/theme";
 
 interface ISideBar {
   setCategory: (e: string) => void;
+  currentCategory: string;
 }
 
-function SideBar({ setCategory }: ISideBar) {
-  const ref = useRef<HTMLDivElement>(null);
-
-  const SidebarControl = () => {
-    console.log(window.innerHeight);
-    console.log(document.documentElement.clientHeight);
-
-    const origin_y = ref.current?.getBoundingClientRect().top;
-
-    const pos_y = window.scrollY;
-    const obj = ref.current?.style;
-
-    if (!obj) return;
-    if (!origin_y) return;
-
-    if (pos_y > (window.innerHeight * 7) / 10) {
-      ref.current.style.position = "fixed";
-      ref.current.style.top = "80px";
-    } else {
-      ref.current.style.position = "absolute";
-      ref.current.style.left = "0";
-      ref.current.style.top = "70vh";
-    }
-  };
-
-  useEffect(() => {
-    window.addEventListener("scroll", SidebarControl);
-
-    return () => window.removeEventListener("scroll", SidebarControl);
-  }, []);
-
+function SideBar({ setCategory, currentCategory }: ISideBar) {
   return (
-    <div
-      ref={ref}
-      css={css`
-        position: absolute;
-        left: 0;
-        width: 10%;
-        max-width: 180px;
-      `}
-    >
-      <Styled.Item onClick={() => setCategory("all")}>All</Styled.Item>
-      <Styled.Item onClick={() => setCategory("git")}>
-        Git & Githubs
-      </Styled.Item>
-      <Styled.Item onClick={() => setCategory("fe")}>Front-End</Styled.Item>
-      <Styled.Item onClick={() => setCategory("be")}>Back-End</Styled.Item>
-      <Styled.Item onClick={() => setCategory("other")}>기타</Styled.Item>
-    </div>
+    <Styled.Wrapper>
+      {Object.entries(CATEGORY_TEXT).map((entry) => (
+        <Styled.Item
+          css={css`
+            background-color: ${currentCategory == entry[0] &&
+            theme.color.pink};
+          `}
+          onClick={() => setCategory(entry[0])}
+        >
+          {entry[1]}
+        </Styled.Item>
+      ))}
+    </Styled.Wrapper>
   );
 }
 
