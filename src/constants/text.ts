259976d475467fd8fdce259acd76f3db26d98fe8
wuzoo <wndyd9911@gmail.com>
2024-01-29 11:50:28 +0900

    refactor: 과제 제출 페이지 subtitle 텍스트 변경

--- src/constants/text.ts
+++ src/constants/text.ts
@@ -4,9 +4,8 @@ export const TEXT: { [key: string]: string } = {
   greeting: "님 반갑습니다",
   contact: "사자들의 정보를 볼 수 있어요.",
   notification: "어떤 일이 기다리고 있을까요 ?",
-  assignlist: "나의 과제 목록을 확인하세요",
-  assignsubmit:
-    "최근 업로드된 다른 아기사자의 과제와 지금까지 업로드된 과제들을 구경할 수 있어요.",
+  assignlist: "나의 과제 목록을 확인하세요.",
+  assignsubmit: "다른 아기사자들의 과제도 한 번 구경해보세요.",
   "q&a": "깨달음은 호기심으로부터.",
   profile: "사자들에게 보여질 프로필을 수정할 수 있어요.",
 };
@@ -20,5 +19,5 @@ export const SUB_TEXT: { [key: string]: string } = {
   qnatitle: "제목을 입력해주세요.",
   qnatags: "질문 내용에 해당하는 태그를 선택해주세요.",
   qnacode: "코드를 첨부하려면 캡쳐하여 올려주세요.",
-  qnacontent: "궁금한 점을 자세히 알려주세요",
+  qnacontent: "궁금한 점을 자세히 알려주세요.",
 };
