ac87728059756ba0c5a5d9c044630236d45450aa
wuzoo <wndyd9911@gmail.com>
2024-02-02 21:23:30 +0900

    feat: auth 인증 후 access 토큰으로 axios 인증 기본 설정

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,26 +1,85 @@
 import * as Styled from "./style";
-import Header from "../../components/Header/Header";
 import Typo from "../../components/Typo/Typo";
 import PageLogo from "../../components/PageLogo/PageLogo";
 import Button from "../../components/Button/Button";
 import { TEXT } from "../../constants/text";
+import { css } from "@emotion/react";
+import axios from "axios";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 function Login() {
+  const [id, setId] = useState("");
+  const [pwd, setPwd] = useState("");
+  const navigate = useNavigate();
+
+  const onLogin = async (email: string, password: string) => {
+    const data = {
+      email,
+      password,
+    };
+    await axios
+      .post("/auth/login", data)
+      .then((res) => {
+        const { accessToken } = res.data;
+        axios.defaults.headers.common[
+          "Authorization"
+        ] = `Bearer ${accessToken}`;
+
+        if (res.status === 200) {
+          navigate("/", {
+            state: {
+              isTokenloaded: true,
+            },
+          });
+        }
+        if (res.status === 401) {
+          // showToast()
+        }
+      })
+      .catch((err) => {
+        throw new Error("error");
+      });
+  };
+
+  const handleLogin = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    onLogin(id, pwd);
+  };
+
   return (
     <>
       <PageLogo width="350" height="350" type="login" left="22%" top="23%" />
-      <Header type="login" />
       <Styled.page>
         <Styled.loginbox>
           <Styled.welcometext>{TEXT.login}</Styled.welcometext>
-          <Styled.Input placeholder="아이디를 입력하세요" />
-          <Styled.Input placeholder="비밀번호를 입력하세요" />
+          <Styled.Form
+            onSubmit={(e) => handleLogin(e)}
+            css={css`
+              display: flex;
+              flex-direction: column;
+              align-items: center;
+            `}
+          >
+            <Styled.Input
+              type="text"
+              value={id}
+              onChange={(e) => setId(e.target.value)}
+              placeholder="아이디를 입력하세요"
+            />
+            <Styled.Input
+              type="password"
+              value={pwd}
+              onChange={(e) => setPwd(e.target.value)}
+              placeholder="비밀번호를 입력하세요"
+            />
 
-          <Button width="60%" height="40" onClick={() => {}}>
-            <Typo fontSize="20" weight="600" color="white">
-              로그인
-            </Typo>
-          </Button>
+            <Button type="submit" width="60%" height="40px" onClick={() => {}}>
+              <Typo fontSize="20" weight="600" color="white">
+                로그인
+              </Typo>
+            </Button>
+          </Styled.Form>
         </Styled.loginbox>
       </Styled.page>
     </>
