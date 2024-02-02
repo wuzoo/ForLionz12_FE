4bb637b5e519f0ab782d7644ea1e9f3979df078e
wuzoo <wndyd9911@gmail.com>
2024-02-02 22:50:35 +0900

    style: Layout 스타일 컴포넌트로 수정

---
+++ src/components/Layout/style.ts
@@ -0,0 +1,10 @@
+import styled from "@emotion/styled";
+
+export const Layout = styled.div`
+  margin: 80px 0px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  max-width: 100vw;
+  position: relative;
+`;
