1c1cd73f4ac9183f2edb4c4e470a8108ea20d205
wuzoo <wndyd9911@gmail.com>
2024-02-03 19:18:59 +0900

    feat: useAllNotification 훅으로 데이터 fetch 후 필터링 + 반영하기

--- src/pages/Notification/Notification.tsx
+++ src/pages/Notification/Notification.tsx
@@ -1,36 +1,44 @@
 import Banner from "../../components/Banner/Banner";
 import PartToggle from "../../components/PartToggle/PartToggle";
-import useSelectedPart from "../../hooks/useSelectedPart";
 import ListItem from "../../components/ListItem/NoticeIndex/ListItem";
 import * as Styled from "./style";
+import { useState } from "react";
+import useAllNotification from "../../hooks/api/notification/useAllNotification";
+import getFormedDate from "../../utils/getFormedDate";
+import NoticeDetail from "./components/Notice-detail";
 
 function Notification() {
-  const [part, feclick, beclick, allclick] = useSelectedPart("notice");
+  const [selectedPart, setSelectedPart] = useState("all");
+  const [clickedId, setClickedId] = useState(0);
+
+  const { isloading, error, data } = useAllNotification();
+
+  const filteredData = data?.filter(
+    (item) => item.part === selectedPart.toUpperCase()
+  );
+
+  console.log(data);
 
   return (
     <Styled.Wrapper>
       <Banner type="notification" logowidth="500" logoheight="500" />
       <Styled.Toggle>
-        <PartToggle
-          part={part}
-          showfe={feclick}
-          showbe={beclick}
-          showother={allclick}
-          flag={true}
-        />
+        <PartToggle part={selectedPart} setPart={setSelectedPart} />
       </Styled.Toggle>
       <Styled.Items>
-        <ListItem
-          part="fe"
-          content="멋쟁이사자처럼은 지금까지의 일정을 모두 뒤로하고 폐지하기로 결정하였습니다. 죄송합니다."
-          date="2023-04-10 19:00:00"
-        />
-        <ListItem
-          part="be"
-          content="저 Spring 모릅니다.. 저한테 물어보시면 큰일나요."
-          date="2023-04-10 19:00:00"
-        />
+        {filteredData?.map((item) => (
+          <ListItem
+            onClick={() => setClickedId(item.id)}
+            id={item.id}
+            part={item.part.toLowerCase()}
+            title={item.title}
+            date={getFormedDate(item.createdAt)}
+          />
+        ))}
       </Styled.Items>
+      {clickedId !== 0 && (
+        <NoticeDetail clickedId={clickedId} setClickedId={setClickedId} />
+      )}
     </Styled.Wrapper>
   );
 }
