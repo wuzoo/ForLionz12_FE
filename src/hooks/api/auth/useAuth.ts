d7e0ad76b00e447379cd19abbc05401bd0b6395b
wuzoo <wndyd9911@gmail.com>
2024-02-03 18:57:06 +0900

    feat: 로그인 form 제출 시 check 함수 실행, 임시로 토큰 로컬스토리지 저장 + dispatch 유저 업데이트

--- src/hooks/api/auth/useAuth.ts
+++ src/hooks/api/auth/useAuth.ts
@@ -1,9 +1,11 @@
 import axios from "axios";
 import { useCallback } from "react";
 import { useNavigate } from "react-router-dom";
+import { useLoginInfoDispatch } from "../../../context/LoginUser/User";
 
 function useAuth() {
   const navigate = useNavigate();
+  const dispatch = useLoginInfoDispatch();
 
   const check = useCallback(
     async (email: string, password: string) => {
@@ -14,11 +16,16 @@ function useAuth() {
       await axios
         .post("/auth/login", data)
         .then((res) => {
-          const { accessToken } = res.data;
+          console.log(res);
+
+          const { accessToken } = res.data.data;
+
           axios.defaults.headers.common[
             "Authorization"
           ] = `Bearer ${accessToken}`;
 
+          localStorage.setItem("accessToken", accessToken);
+          console.log(accessToken);
           if (res.status === 200) {
             navigate("/");
           }
@@ -29,6 +36,17 @@ function useAuth() {
         .catch((err) => {
           throw new Error(err);
         });
+
+      const response = await axios.get("/member/me");
+
+      const res = response.data.data;
+      console.log(response.data.data);
+      dispatch({
+        type: "LOGIN",
+        data: {
+          ...res,
+        },
+      });
     },
     [navigate]
   );
