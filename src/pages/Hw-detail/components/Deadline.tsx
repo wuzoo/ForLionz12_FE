9c6e172ce06b95653b61cb3d1c712da0b61c1e2f
wuzoo <wndyd9911@gmail.com>
2024-02-03 15:36:39 +0900

    feat: Deadline 컴포넌트 생성 및 구현

---
+++ src/pages/Hw-detail/components/Deadline.tsx
@@ -0,0 +1,62 @@
+import { css } from "@emotion/react";
+import Typo from "../../../components/Typo/Typo";
+
+type DateType = {
+  expireAt: string;
+};
+
+function Deadline({ expireAt }: DateType) {
+  console.log(new Date(expireAt));
+
+  const start = new Date();
+  const end = new Date(expireAt);
+
+  const gap = end.getTime() - start.getTime();
+  console.log(gap);
+
+  const dayDigit = 1000 * 60 * 60 * 24;
+  const day = Math.floor(gap / (1000 * 60 * 60 * 24));
+
+  const hourDigit = 1000 * 60 * 60;
+  const rest = gap - day * dayDigit;
+  const hour = Math.floor(rest / (1000 * 60 * 60));
+
+  const minuteDigit = 1000 * 60;
+  const minute = Math.floor((rest - hour * hourDigit) / minuteDigit);
+
+  console.log(day, hour, minute);
+
+  return (
+    <div
+      css={css`
+        span {
+          padding: 0px 3px;
+        }
+      `}
+    >
+      <Typo fontSize="22" weight="600">
+        마감까지
+      </Typo>
+      <Typo weight="600" color="darkblue" fontSize="40">
+        {day}
+      </Typo>
+      <Typo fontSize="22" weight="600">
+        일
+      </Typo>
+      <Typo weight="600" color="darkblue" fontSize="40">
+        {hour}
+      </Typo>
+      <Typo fontSize="22" weight="600">
+        시간
+      </Typo>
+      <Typo weight="600" color="darkblue" fontSize="40">
+        {minute}
+      </Typo>
+      <Typo fontSize="22" weight="600">
+        분
+      </Typo>
+    </div>
+  );
+}
+
+export default Deadline;
