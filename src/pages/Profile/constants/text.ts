87b8d278d92eb9620972842ea621f63df6f020bb
wuzoo <wndyd9911@gmail.com>
2024-02-03 18:14:13 +0900

    refactor: insta => instagram

--- src/pages/Profile/constants/text.ts
+++ src/pages/Profile/constants/text.ts
@@ -12,7 +12,7 @@ export const PROFILE_TEXT: { [key: string]: TextObj } = {
     main: "깃허브 주소",
     sub: "나의 깃허브 프로필 주소를 입력합니다.",
   },
-  insta: {
+  instagram: {
     main: "인스타그램 아이디",
     sub: "나의 인스타그램 아이디를 입력합니다.",
   },
