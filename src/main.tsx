5091b64187af5c0011b9309f50f9f5cc39b9741a
wuzoo <wndyd9911@gmail.com>
2024-02-07 20:53:38 +0900

    refactor: env로 base url 적용

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import axios from "axios";
 
-axios.defaults.baseURL = "https://likelioninha.site";
+axios.defaults.baseURL = import.meta.env.VITE_SERVER_URL;
 axios.defaults.withCredentials = true;
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
