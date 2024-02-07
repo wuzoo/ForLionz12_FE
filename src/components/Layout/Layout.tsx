bc9c74ef9a321cc199ef086df9e13e4f07398b8b
wuzoo <wndyd9911@gmail.com>
2024-02-07 18:30:19 +0900

    refactor: token 확인과 update user 함수 분리, useCallback으로 함수 관리

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,39 +1,51 @@
 import { Outlet, useLocation, useNavigate } from "react-router-dom";
 import Header from "../Header/Header";
-import { useEffect } from "react";
+import { useCallback, useEffect } from "react";
 import axios from "axios";
 import * as Styled from "./style";
-import { useLoginInfoState } from "../../context/LoginUser/User";
+import { useLoginInfoDispatch } from "../../context/LoginUser/User";
 
 function Layout() {
   const { pathname } = useLocation();
   const navigate = useNavigate();
   const isLoginMatch = pathname === "/login";
-  const state = useLoginInfoState();
+  const dispatch = useLoginInfoDispatch();
 
   useEffect(() => {
     window.scrollTo(0, 0);
-    console.log(state);
   }, [pathname]);
 
   useEffect(() => {
-    checkTmp();
+    checkToken();
+    updateUser();
   }, []);
 
-  const checkTmp = async () => {
+  const checkToken = useCallback(async () => {
     console.log("reloaded");
 
     const token = localStorage.getItem("accessToken");
-    console.log(token);
+
     if (!token) {
       navigate("/login");
     }
     axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
-  };
+  }, []);
+
+  const updateUser = useCallback(async () => {
+    const response = await axios.get("/member/me");
+    const data = response.data.data;
+
+    dispatch({
+      type: "LOGIN",
+      data: {
+        ...data,
+      },
+    });
+  }, []);
 
   return (
     <Styled.Layout>
-      <Header profile={state.imageUrl} type={isLoginMatch ? "login" : ""} />
+      <Header type={isLoginMatch ? "login" : ""} />
       <Outlet />
     </Styled.Layout>
   );
