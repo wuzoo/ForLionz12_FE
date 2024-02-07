c593cc06c6a09afae668198a9f708d3ccee7b740
wuzoo <wndyd9911@gmail.com>
2024-02-07 18:34:22 +0900

    refactor: default user svg로 user의 imageUrl이 없을 시 기본이미지 표현

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -2,8 +2,6 @@ import { User } from "./style";
 import { IProfile } from "./types";
 import user from "../../assets/imgs/defaultUser.svg";
 
-function Profile(props: IProfile) {
+export default function Profile(props: IProfile) {
   return <User src={props.url || user} {...props} />;
 }
-
-export default Profile;
