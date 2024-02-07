310408e99d657278c2d6d80ceb1fb0d78927ffd2
wuzoo <wndyd9911@gmail.com>
2024-02-07 20:56:28 +0900

    feat: member api 응답 관련 type 작성

--- src/types/User.ts
+++ src/types/User.ts
@@ -3,7 +3,12 @@ export interface IUserInfo {
   email: string;
   name: string;
   part: string;
+  imageUrl: string;
   introduction: string;
   githubAddress: string;
   instagramId: string;
+}
+
+export interface IMembers extends Array<IUserInfo> {
+  members: IUserInfo[];
 }
