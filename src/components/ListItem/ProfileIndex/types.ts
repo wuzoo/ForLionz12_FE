9d4123e6cee98e657075a7a3c78e172343cf050b
wuzoo <wndyd9911@gmail.com>
2024-02-07 20:35:35 +0900

    refactor: info, setInfo를 prop으로 넘기지 않고, 컴포넌트를 분리하기로 결정

--- src/components/ListItem/ProfileIndex/types.ts
+++ src/components/ListItem/ProfileIndex/types.ts
@@ -2,7 +2,5 @@ import { SetStateAction } from "react";
 
 export interface IItem {
   type: string;
-  info: string;
-  setInfo: React.Dispatch<SetStateAction<string>>;
   setSubmited: React.Dispatch<SetStateAction<boolean>>;
 }
