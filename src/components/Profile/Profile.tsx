5ab9dc632db8467b036575ff8448fedb58356106
wuzoo <wndyd9911@gmail.com>
2024-02-07 15:41:25 +0900

    refactor: div -> img, default user img로 url prop이 없을 때 해당 이미지 지정

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -1,8 +1,9 @@
 import { User } from "./style";
 import { IProfile } from "./types";
+import user from "../../assets/imgs/defaultUser.svg";
 
 function Profile(props: IProfile) {
-  return <User {...props} />;
+  return <User src={props.url || user} {...props} />;
 }
 
 export default Profile;
