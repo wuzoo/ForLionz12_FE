6eb38c14347bf0dfcd02b34140e6e95d401315e3
wuzoo <wndyd9911@gmail.com>
2024-02-12 17:23:33 +0900

    feat: 부모 태그가 달라질때마다 자식태그 + data 호출 + 자식 태그가 달라질 때마다 n개의 태그에 해당하는 데이터 반환하는 커스텀 훅(query: 중복 제거된 선택된 자식 태그 배열)

---
+++ src/hooks/api/qna/useSelectedData.ts
@@ -0,0 +1,47 @@
+import { useEffect, useState } from "react";
+import {
+  getAllQuestion,
+  getChildTagData,
+  getParentTagData,
+} from "../../../api/qna";
+import { ChildtagType, IQna } from "../../../types/Qna";
+
+function useSelectedData(id: number) {
+  const [data, setData] = useState<IQna[]>();
+  const [childTags, setChildTags] = useState<ChildtagType[]>();
+
+  const [query, setQuery] = useState<number[]>([]);
+
+  const get = async () => {
+    if (id === 0) {
+      const response = await getAllQuestion();
+      setData(response.data);
+    } else {
+      const response = await getParentTagData(id);
+      const { childTags, questionPosts } = response.data;
+      setChildTags(childTags);
+      setData(questionPosts);
+    }
+  };
+
+  const getChildData = async () => {
+    const response = await getChildTagData(query);
+
+    if (response !== null) {
+      setData(response.data);
+    }
+  };
+
+  useEffect(() => {
+    get();
+    setQuery([]);
+  }, [id]);
+
+  useEffect(() => {
+    getChildData();
+  }, [query]);
+
+  return { childTags, data, query, setQuery };
+}
+
+export default useSelectedData;
