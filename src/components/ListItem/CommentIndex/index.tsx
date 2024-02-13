552c6aa0a585bfe08f51f07005ab77a7c2a55643
wuzoo <wndyd9911@gmail.com>
2024-02-14 00:43:38 +0900

    feat: 특정 질문글 댓글 컴포넌트 생성 및 대댓글 데이터 useChildComments 커스텀 훅으로 가져오기

--- src/components/ListItem/CommentIndex/index.tsx
+++ src/components/ListItem/CommentIndex/index.tsx
@@ -6,98 +6,78 @@ import chat from "./assets/chat.svg";
 import getFormedDate from "../../../utils/getFormedDate";
 import useChildComments from "../../../hooks/api/comment/useChildComments";
 import { css } from "@emotion/react";
-import { theme } from "../../../theme/theme";
+import Button from "../../Button/Button";
+import Item from "./components/Child";
+import React, { useState } from "react";
+import { IComment } from "./types";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
-interface IComment {
-  id: number;
-  url: string;
-  name: string;
-  createdAt: string;
-  content: string;
-  part: string;
-  type: "parent" | "child";
-  last: boolean;
-}
-
-function ListItem({ url, name, createdAt, id, content, part, type }: IComment) {
+function ListItem({ url, name, createdAt, id, content, part }: IComment) {
   const { data } = useChildComments(id);
+  const [isChildClicked, setIsChildClicked] = useState(false);
+  const [comment, setComment] = useState("");
 
-  console.log(data);
+  const navigate = useNavigate();
+
+  const handleAddComment = async (e: React.FormEvent<HTMLFormElement>) => {};
 
   return (
-    <Styled.Wrapper
-      css={css`
-        border-bottom: ${type === "parent"
-          ? `1px solid ${theme.color.lightgray}`
-          : "none"};
-        padding: ${type === "parent" ? 0 : "0px 20px"};
-      `}
-    >
-      <Styled.Info>
-        <Profile url={url || user} size="60" />
-        <Styled.NameAndDate>
-          <Styled.Name>
-            <Typo>
-              {name} <Typo>({part})</Typo>
-            </Typo>
-          </Styled.Name>
-          <Styled.Date>
-            <Typo fontSize="14" color="darkgray" weight="500">
-              {getFormedDate(createdAt)}
-            </Typo>
-          </Styled.Date>
-        </Styled.NameAndDate>
-      </Styled.Info>
+    <Styled.Wrapper css={css``}>
+      <div>
+        <Styled.Info>
+          <Profile url={url || user} size="60" />
+          <Styled.NameAndDate>
+            <Styled.Name>
+              <Typo>
+                {name} <Typo>({part})</Typo>
+              </Typo>
+            </Styled.Name>
+            <Styled.Date>
+              <Typo fontSize="14" color="darkgray" weight="500">
+                {getFormedDate(createdAt)}
+              </Typo>
+            </Styled.Date>
+          </Styled.NameAndDate>
+        </Styled.Info>
 
-      <Styled.Comment
-        css={css`
-          border-bottom: ${type === "child" &&
-          `1px solid ${theme.color.lightgray}`};
-        `}
-      >
-        <Typo weight="500">{content}</Typo>
-      </Styled.Comment>
+        <Styled.Comment>
+          <Typo weight="500">{content}</Typo>
+        </Styled.Comment>
 
-      <Styled.Child
-        css={css`
-          display: ${type === "child" && "none"};
-        `}
-      >
-        <img width="18" src={chat} />
-        <Typo color="darkblue" weight="600">
-          {data?.length ? `${data?.length}개의 답글` : "댓글 달기"}
-        </Typo>
-      </Styled.Child>
+        <Styled.Child onClick={() => setIsChildClicked((prev) => !prev)}>
+          <img width="18" src={chat} />
+          <Typo color="darkblue" weight="600">
+            {data?.length ? `${data?.length}개의 답글` : "댓글 달기"}
+          </Typo>
+        </Styled.Child>
+      </div>
       <div
         css={css`
-          display: flex;
+          display: ${isChildClicked ? "flex" : "none"};
           flex-direction: column;
           align-items: center;
-          width: 100%;
         `}
       >
-        {data?.map((item, index) => (
-          <>
-            <ListItem
-              last={index + 1 === data.length}
-              type="child"
-              id={item.commentId}
-              name={item.name}
-              part={item.part}
-              createdAt={item.createdAt}
-              content={item.content}
-              url={item.memberImageUrl}
-            />
-          </>
+        {data?.map((item) => (
+          <Item
+            name={item.name}
+            part={item.part}
+            createdAt={item.createdAt}
+            content={item.content}
+            url={item.memberImageUrl}
+          />
         ))}
-        {/* <Styled.Form
-          css={css`
-            display: ${(type === "parent" || !last) && "none"};
-          `}
-        >
-          <Styled.ChildCommentInput placeholder="댓글을 작성해주세요." />
-          <Button color="white">댓글 작성</Button>
-        </Styled.Form> */}
+        <Styled.Form onSubmit={handleAddComment}>
+          <Styled.ChildCommentInput
+            value={comment}
+            onChange={(e) => setComment(e.target.value)}
+            placeholder="댓글을 작성해주세요."
+          />
+          <Button type="submit" width="100px" color="white">
+            댓글 작성
+          </Button>
+        </Styled.Form>
       </div>
     </Styled.Wrapper>
   );
