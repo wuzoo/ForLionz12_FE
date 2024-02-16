bdbaf315863a9c94d7a1973ef4079a203a8ff4c1
wuzoo <wndyd9911@gmail.com>
2024-02-16 22:25:25 +0900

    refactor: 모든 key string 네이밍 일치 + interface 생성

--- src/constants/text.ts
+++ src/constants/text.ts
@@ -1,23 +1,47 @@
-export const TEXT: { [key: string]: string } = {
-  login: "LIKELION\nINHA UNIV 12기와\n함께하기",
-  main: "Growl\nTo Growth\nWelcome",
-  greeting: "님 반갑습니다",
-  contact: "사자들의 정보를 볼 수 있어요.",
-  notification: "어떤 일이 기다리고 있을까요 ?",
-  assignlist: "나의 과제 목록을 확인하세요.",
-  assignsubmit: "다른 아기사자들의 과제도 구경해보세요.",
-  "q&a": "깨달음은 호기심으로부터.",
-  profile: "사자들에게 보여질 프로필을 수정할 수 있어요.",
+export const TEXT: IText = {
+  LOGIN: "LIKELION\nINHA UNIV 12기와\n함께하기", //
+  HOME_MAIN_TEXT: "Growl\nTo Growth\nWelcome", //
+  HOME_GREETING: "님 반갑습니다", //
+  CONTACT: "사자들의 정보를 볼 수 있어요.",
+  NOTIFICATION: "어떤 일이 기다리고 있을까요 ?", //
+  HW_LIST: "나의 과제 목록을 확인하세요.", //
+  HW_SUBMIT: "과제 제출 페이지입니다. 다른 아기사자들의 과제도 구경해보세요.", //
+  "Q&A": "깨달음은 호기심으로부터.",
+  PROFILE: "사자들에게 보여질 프로필을 수정할 수 있어요.", //
 };
 
-export const SUB_TEXT: { [key: string]: string } = {
-  otherparthw: "다른 파트의 과제들을 구경할 수 있어요.",
-  recentupload: "최근 업로드된 아기사자의 과제를 구경하세요 !",
-  hwexplain: "짧게 적어도 됩니다. 자신의 과제를 마음껏 표현해주세요.",
-  hwlink: "과제 깃허브 링크 또는 배포 링크",
-  hwother: "아기사자들의 과제들을 구경해요.",
-  qnatitle: "제목을 입력해주세요.",
-  qnatags: "질문 내용에 해당하는 태그를 선택해주세요.",
-  qnacode: "코드를 첨부하려면 캡쳐하여 올려주세요.",
-  qnacontent: "궁금한 점을 자세히 알려주세요.",
+export const SUB_TEXT: ISubText = {
+  HW_OTHER_PART: "다른 파트의 과제들을 구경할 수 있어요.",
+  HW_RECENT_UPLOAD: "최근 업로드된 아기사자의 과제를 구경하세요 !",
+  HW_EXPLAIN: "짧게 적어도 됩니다. 자신의 과제를 마음껏 표현해주세요.",
+  HW_LINK: "과제 깃허브 링크 또는 배포 링크",
+  HW_OTHER: "아기사자들의 과제들을 구경해요.",
+  QNA_TITLE: "제목을 입력해주세요.",
+  QNA_TAGS: "질문 내용에 해당하는 태그를 선택해주세요.",
+  QNA_CONTENT: "궁금한 점을 자세히 알려주세요.",
 };
+
+interface ISubText {
+  [key: string]: string;
+  HW_OTHER_PART: string;
+  HW_RECENT_UPLOAD: string;
+  HW_EXPLAIN: string;
+  HW_LINK: string;
+  HW_OTHER: string;
+  QNA_TITLE: string;
+  QNA_TAGS: string;
+  QNA_CONTENT: string;
+}
+
+interface IText {
+  [key: string]: string;
+  LOGIN: string;
+  HOME_MAIN_TEXT: string;
+  HOME_GREETING: string;
+  CONTACT: string;
+  NOTIFICATION: string;
+  HW_LIST: string;
+  HW_SUBMIT: string;
+  "Q&A": string;
+  PROFILE: string;
+}
