3883f5b088d6c6a53f2cf433343e30b06659baaa
wuzoo <wndyd9911@gmail.com>
2024-02-16 17:15:45 +0900

    style: header 10, overlay 11, modal 12

--- src/pages/Notification/components/style.ts
+++ src/pages/Notification/components/style.ts
@@ -9,21 +9,22 @@ export const Overlay = styled(motion.div)`
   height: 100%;
   background-color: rgba(0, 0, 0, 0.5);
   opacity: 0;
+  z-index: 11;
 `;
 
 export const Modal = styled(motion.div)`
   position: fixed;
   background-color: ${(props) => props.theme.color.white};
   border-radius: 30px;
-  width: 70vw;
-  height: 80vh;
+  width: 70%;
+  height: 80%;
   top: 0;
   bottom: 0;
   left: 0;
   right: 0;
   margin: auto auto;
   padding: 80px;
-
+  z-index: 12;
   display: flex;
   flex-direction: column;
 `;
