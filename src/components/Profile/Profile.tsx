529a31ab0b8ba03900aed58b87d41c53f724bef5
wuzoo <wndyd9911@gmail.com>
2024-02-16 23:07:21 +0900

    refactor: import directory 수정

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -1,6 +1,6 @@
 import { User } from "./style";
 import { IProfile } from "./types";
-import user from "../../assets/icons/default-user/defaultUser.svg";
+import user from "../../assets/icons/user/defaultUser.svg";
 
 export default function Profile(props: IProfile) {
   return <User src={props.url || user} {...props} />;
