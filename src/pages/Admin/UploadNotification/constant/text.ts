3f7f47e3d99b4a0060ef0193107a46f4d0d5c2e8
wuzoo <wndyd9911@gmail.com>
2024-02-16 21:40:29 +0900

    feat: 공지사항 업로드 제목 및 설명 text 상수 선언

---
+++ src/pages/Admin/UploadNotification/constant/text.ts
@@ -0,0 +1,11 @@
+export const TEXT: { [key: string]: string } = Object.freeze({
+  title: "제목을 입력해주세요.",
+  part: "ALL/FE/BE",
+  content: "공지 내용을 작성해주세요.",
+});
+
+export const TITLE: { [key: string]: string } = Object.freeze({
+  title: "Title",
+  part: "Part",
+  content: "Content",
+});
