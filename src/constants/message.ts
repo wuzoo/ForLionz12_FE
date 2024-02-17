5aad1b4b5112599401db85f34c2509f11c62990c
wuzoo <wndyd9911@gmail.com>
2024-02-17 18:08:48 +0900

    feat: error message 추가

--- src/constants/message.ts
+++ src/constants/message.ts
@@ -3,9 +3,13 @@ export const ERROR = Object.freeze({
   NO_AUTH: "유저 정보가 올바르지 않습니다.",
   CODE500: "정보를 불러오지 못했습니다.",
 
+  ROUTE_NO_PARAM: "잘못된 경로 입니다.",
+
   ASSIGNMENT_UPLOAD: "과제 업로드 에러",
   NOTIFICATION_UPLOAD: "공지사항 업로드 에러",
   COMMENT_UPLOAD: "댓글 작성 에러",
+  QNA_UPLOAD: "큐앤에이 업로드 에러",
+  FILE_UPLOAD: "파일 업로드에 실패하였습니다.",
 
   ALL_MEMBER: "모든 멤버 조회 에러",
   ALL_NOTICE: "모든 공지사항 조회 에러",
@@ -16,12 +20,18 @@ export const ERROR = Object.freeze({
   ID_ASSIGNMENT: "특정 과제 조회 에러",
   ID_NOTIFICATION: "특정 공지사항 조회 에러",
   ID_QNA: "특정 큐앤에이 조회 에러",
+  ID_MEMBER: "특정 유저 조회 에러",
 
+  DELETE_QNA: "해당 큐앤에이 삭제에 실패하였습니다.",
+
+  NO_LINK_ASSIGNMENT: "링크는 필수 사항입니다.",
   PART_ASSIGNMENT: "해당 파트 과제 조회 에러",
-  SUBMIT_ASSIGNMENT: "제출된 과제 조회 에러",
+  SUBMITTED_ASSIGNMENT: "제출된 과제 조회 에러",
+  CANT_SUBMIT_ASSIGNMENT: "제출에 실패하였습니다.",
 
   MY_ASSIGNMENT: "내 제출 조회 에러",
   MY_INFO: "내 정보 조회 에러",
+  UPDATE_INFO: "내 정보 업데이트에 실패하였습니다.",
 
   NO_PART: "파트가 존재하지 않습니다.",
   NO_ID: "유저 아이디가 존재하지 않습니다.",
