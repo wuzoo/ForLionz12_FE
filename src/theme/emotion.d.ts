0c304e352ba3a133b9e668ca34cf38b444948351
wuzoo <wndyd9911@gmail.com>
2024-02-17 20:01:49 +0900

    feat: const assertion하여 theme export

--- src/theme/emotion.d.ts
+++ src/theme/emotion.d.ts
@@ -1,41 +1,8 @@
 import "@emotion/react";
+import { theme } from "./theme";
+
+type MyTheme = typeof theme;
 
 declare module "@emotion/react" {
-  export interface Theme {
-    color: {
-      [key: string]: string;
-      yellow: string;
-      pink: string;
-      darkpink: string;
-      skyblue: string;
-      darkblue: string;
-      lightgray: string;
-      darkgray: string;
-      white: string;
-      black: string;
-    };
-
-    weight: {
-      [key: string]: string;
-      medium: string;
-      regular: string;
-      semibold: string;
-      bold: string;
-      ultrabold: string;
-    };
-
-    flexRow: (j?: JustifyType, a?: AlignType, gap?: number) => string;
-    flexColumn: (j?: JustifyType, a?: AlignType, gap?: number) => string;
-  }
+  interface Theme extends MyTheme {}
 }
-
-type JustifyType =
-  | "center"
-  | "space-between"
-  | "space-around"
-  | "end"
-  | "start"
-  | "flex-end"
-  | "flex-start"
-  | "";
-type AlignType = "center" | "end" | "start" | "";
--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -1,6 +1,15 @@
-import { Theme } from "@emotion/react";
+type JustifyType =
+  | "center"
+  | "space-between"
+  | "space-around"
+  | "end"
+  | "start"
+  | "flex-end"
+  | "flex-start"
+  | "";
+type AlignType = "center" | "end" | "start" | "";
 
-export const theme: Theme = {
+const color = {
   color: {
     yellow: "#FDD84E",
     pink: "#FAB6AD",
@@ -14,31 +23,38 @@ export const theme: Theme = {
     darkgray: "#8E8888",
     black: "#000000",
   },
+} as const;
 
-  weight: {
-    regular: "400",
-    medium: "500",
-    semibold: "600",
-    bold: "700",
-    ultrabold: "800",
-  },
+const weight = {
+  regular: "400",
+  medium: "500",
+  semibold: "600",
+  bold: "700",
+  ultrabold: "800",
+} as const;
 
-  flexRow(j = "", a = "", gap = 0) {
-    return `
-      display: flex;
-      justify-content: ${j};
-      align-items: ${a};
-      gap: ${gap}px;
-      `;
-  },
-
-  flexColumn(j = "", a = "", gap = 0) {
-    return `
+function flexRow(j: JustifyType = "", a: AlignType = "", gap = 0) {
+  return `
     display: flex;
-    flex-direction: column;
-    align-items: ${a};
     justify-content: ${j};
+    align-items: ${a};
     gap: ${gap}px;
     `;
-  },
-};
+}
+
+function flexColumn(j: JustifyType = "", a: AlignType = "", gap = 0) {
+  return `
+  display: flex;
+  flex-direction: column;
+  align-items: ${a};
+  justify-content: ${j};
+  gap: ${gap}px;
+  `;
+}
+
+export const theme = {
+  color,
+  weight,
+  flexRow,
+  flexColumn,
+} as const;
