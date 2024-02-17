54970ecf22d48a63b6eb9615c41b5992a8086953
wuzoo <wndyd9911@gmail.com>
2024-02-18 00:25:27 +0900

    feat: children prop을 Markdown으로 리턴하는 컴포넌트 생성

---
+++ src/pages/QnaDetail/components/Markdown.tsx
@@ -0,0 +1,47 @@
+import ReactMarkdown from "react-markdown";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { oneDark } from "react-syntax-highlighter/dist/cjs/styles/prism";
+import styled from "@emotion/styled";
+
+const MDImg = styled.img`
+  max-width: 100%;
+  height: 400px;
+  object-fit: cover;
+`;
+
+const MDParagraph = styled.p`
+  line-height: 1.3;
+`;
+
+export default function Markdown({ children }: { children: string }) {
+  return (
+    <ReactMarkdown
+      children={children}
+      components={{
+        code: (props: any) => {
+          const match = /language-(\w+)/.exec(props.className || "");
+          return (
+            <SyntaxHighlighter
+              children={String(props.children).replace(/\n$/, "")}
+              style={oneDark}
+              language={match && match[1]}
+              PreTag="div"
+              showLineNumbers={true}
+              {...props}
+            />
+          );
+        },
+        p: (props: any) => {
+          return (
+            <>
+              <MDParagraph>{props.children}</MDParagraph>
+            </>
+          );
+        },
+        img: (props: any) => {
+          return <MDImg {...props} />;
+        },
+      }}
+    />
+  );
+}
