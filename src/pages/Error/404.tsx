86864c140929ba4a1c50b516ac2e33eeb266ca4d
wuzoo <wndyd9911@gmail.com>
2024-02-19 21:21:21 +0900

    refactor: 불필요 코드 제거

--- src/pages/Error/404.tsx
+++ src/pages/Error/404.tsx
@@ -3,16 +3,12 @@ import code404 from "./assets/404.png";
 import { theme } from "../../styles/theme/theme";
 import Typo from "../../components/Typo/Typo";
 import Header from "../../layout/Header/Header";
-import { useErrorDispatch } from "../../context/Error/Error";
-import { ERROR } from "../../constants/message";
 
 export default function NoExist() {
-  const dispatch = useErrorDispatch();
   return (
     <>
       <Header />
       <div
-        onClick={() => dispatch(ERROR.ALL_ASSIGNMENT)}
         css={css`
           margin-top: 30vh;
           ${theme.flexColumn("end", "center")}
