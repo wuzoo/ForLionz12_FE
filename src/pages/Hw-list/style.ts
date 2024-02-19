e78d32e5f06f15a34439b7979f525084c0e32a85
wuzoo <wndyd9911@gmail.com>
2024-02-19 17:21:04 +0900

    style: Container와 안의 card div에 대한 반응형 디자인 생성

--- src/pages/Hw-list/style.ts
+++ src/pages/Hw-list/style.ts
@@ -25,13 +25,21 @@ export const OtherHWContainer = styled.div`
   margin-top: 24px;
   display: grid;
   grid-template-columns: repeat(3, 1fr);
-  column-gap: 30px;
-  row-gap: 50px;
+  justify-content: space-between;
+  gap: 30px;
 
   @media screen and (max-width: 1400px) {
     grid-template-columns: repeat(2, 1fr);
+    & > div {
+      min-width: 400px;
+      min-height: 400px;
+    }
   }
-  @media screen and (max-width: 900px) {
+  @media screen and (max-width: 920px) {
     grid-template-columns: repeat(1, 1fr);
+    & > div {
+      min-height: 500px;
+      min-width: 500px;
+    }
   }
 `;
