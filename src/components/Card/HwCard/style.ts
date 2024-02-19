9267016ed73feeaeb4fc567f9c624c94913de405
wuzoo <wndyd9911@gmail.com>
2024-02-19 23:28:00 +0900

    style: title의 1 link ellipsis 추가

--- src/components/Card/HwCard/style.ts
+++ src/components/Card/HwCard/style.ts
@@ -29,7 +29,13 @@ export const Content = styled.div`
   ${(props) => props.theme.flexColumn("space-between", "center")}
 `;
 
-export const CardTitle = styled.h1``;
+export const CardTitle = styled.h1`
+  overflow: hidden;
+  text-overflow: ellipsis;
+  display: -webkit-box;
+  -webkit-box-orient: vertical;
+  -webkit-line-clamp: 1;
+`;
 
 export const Badge = styled.div`
   width: 56px;
@@ -42,4 +48,5 @@ export const Date = styled.p``;
 
 export const AlignWrapper = styled.div`
   ${(props) => props.theme.flexRow("space-between", "center")}
+  gap: 20px;
 `;
