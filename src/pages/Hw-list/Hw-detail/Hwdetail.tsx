d7c57ab689d2afc295ecfb6be6cb9aa7eecbc1dc
wuzoo <wndyd9911@gmail.com>
2024-02-20 15:30:36 +0900

    refactor: 상위 컴포넌트 Hw-list에서 useMatch를 통해 모달 컨트롤 -> 필요없는 코드 제거

--- src/pages/Hw-list/Hw-detail/Hwdetail.tsx
+++ src/pages/Hw-list/Hw-detail/Hwdetail.tsx
@@ -1,7 +1,6 @@
 import Typo from "../../../components/Typo/Typo";
 import { Link, useNavigate } from "react-router-dom";
 import * as Styled from "./style";
-import React, { useEffect } from "react";
 import { css } from "@emotion/react";
 import { PART_COLOR } from "../../../constants/partcolor";
 import Button from "../../../components/Button/Button";
@@ -15,140 +14,93 @@ import git from "../../../assets/icons/github/img.svg";
 
 interface IHwDetail {
   clickedId: number;
-  setClickedId: React.Dispatch<React.SetStateAction<number>>;
-  layoutId: string;
 }
 
-function Hwdetail({ clickedId, setClickedId, layoutId }: IHwDetail) {
-  useEffect(() => {
-    document.body.style.maxWidth = `${document.body.clientWidth}px`;
-    document.body.style.overflowY = "hidden";
-
-    return () => {
-      document.body.style.maxWidth = "100vw";
-      document.body.style.overflowY = "";
-    };
-  }, [clickedId]);
-
-  useEffect(() => {
-    window.addEventListener("keydown", handleKeyPress);
-
-    return () => window.removeEventListener("keydown", handleKeyPress);
-  }, []);
-
-  const handleKeyPress = (e: KeyboardEvent) => {
-    const { key } = e;
-
-    if (key === "Escape") {
-      setClickedId(0);
-    }
-  };
-
+function Hwdetail({ clickedId }: IHwDetail) {
   const { data, error } = useGetAssignmentById(clickedId);
   const uid = localStorage.getItem("id");
   const navigate = useNavigate();
-
-  console.log(data);
 
   if (error === "rejected") throw new Error(ERROR.ID_ASSIGNMENT);
   if (!data) return;
 
   const { content, title, createdAt, expireAt, tags, part, id } = data;
 
-  const handleExit = (e: React.MouseEvent<HTMLDivElement>) => {
-    const { target, currentTarget } = e;
-    if (target == currentTarget) {
-      setClickedId(0);
-    }
-  };
-
   return (
     <>
-      <Styled.Overlay
-        onClick={(e) => handleExit(e)}
-        animate={{ opacity: 1 }}
-        exit={{ opacity: 0 }}
-      ></Styled.Overlay>
-      <Styled.Modal
-        transition={{
-          duration: 0.3,
-        }}
-        layoutId={layoutId}
+      <div
+        css={css`
+          ${theme.flexRow("space-between", "", 16)}
+        `}
       >
+        <Styled.MainWrapper>
+          <Styled.TitleAndModifyBtnWrapper>
+            <Styled.Title>
+              <Typo fontSize="44" weight="600">
+                {title}
+              </Typo>
+            </Styled.Title>
+            <AdminModifyBtn
+              type="assignment"
+              uid={uid + ""}
+              id={clickedId + ""}
+            />
+          </Styled.TitleAndModifyBtnWrapper>
+          <Styled.Wrapper>
+            <Styled.Badge
+              css={css`
+                background-color: ${theme.color[
+                  PART_COLOR[part.toLowerCase()]
+                ]};
+              `}
+            >
+              {part.toUpperCase()}
+            </Styled.Badge>
+            <Styled.Date>
+              <Typo fontSize="14" color="darkgray">
+                작성일: {getFormedDate(createdAt)}
+              </Typo>
+            </Styled.Date>
+          </Styled.Wrapper>
+          <Styled.TagWrapper>
+            {tags.map((item) => (
+              <Styled.Tag key={item}>{item}</Styled.Tag>
+            ))}
+          </Styled.TagWrapper>
+        </Styled.MainWrapper>
         <div
           css={css`
-            ${theme.flexRow("space-between", "", 16)}
+            ${theme.flexColumn("", "end", 16)}
           `}
         >
-          <Styled.MainWrapper>
-            <Styled.TitleAndModifyBtnWrapper>
-              <Styled.Title>
-                <Typo fontSize="44" weight="600">
-                  {title}
-                </Typo>
-              </Styled.Title>
-              <AdminModifyBtn
-                type="assignment"
-                uid={uid + ""}
-                id={clickedId + ""}
-              />
-            </Styled.TitleAndModifyBtnWrapper>
-            <Styled.Wrapper>
-              <Styled.Badge
-                css={css`
-                  background-color: ${theme.color[
-                    PART_COLOR[part.toLowerCase()]
-                  ]};
-                `}
-              >
-                {part.toUpperCase()}
-              </Styled.Badge>
-              <Styled.Date>
-                <Typo fontSize="14" color="darkgray">
-                  작성일: {getFormedDate(createdAt)}
-                </Typo>
-              </Styled.Date>
-            </Styled.Wrapper>
-            <Styled.TagWrapper>
-              {tags.map((item) => (
-                <Styled.Tag key={item}>{item}</Styled.Tag>
-              ))}
-            </Styled.TagWrapper>
-          </Styled.MainWrapper>
-          <div
-            css={css`
-              ${theme.flexColumn("", "end", 16)}
-            `}
-          >
-            <Deadline expireAt={expireAt} />
-            <Styled.AssignmentLink>
-              <img src={git} width="50" />
-              <Link to={data?.githubLink}>
-                <Typo color="darkblue">{data.githubLink}</Typo>
-              </Link>
-            </Styled.AssignmentLink>
-          </div>
+          <Deadline expireAt={expireAt} />
+          <Styled.AssignmentLink>
+            <img src={git} width="50" />
+            <Link to={data?.githubLink}>
+              <Typo color="darkblue">{data.githubLink}</Typo>
+            </Link>
+          </Styled.AssignmentLink>
         </div>
-        <Styled.ContentWrapper>
-          <Styled.Content>{content}</Styled.Content>
-        </Styled.ContentWrapper>
-        <div
-          css={css`
-            place-self: center;
-            position: relative;
-            top: 40px;
-          `}
+      </div>
+      <Styled.ContentWrapper>
+        <Styled.Content>{content}</Styled.Content>
+      </Styled.ContentWrapper>
+      <div
+        css={css`
+          place-self: center;
+          position: relative;
+          top: 40px;
+        `}
+      >
+        <Button
+          onClick={() => navigate(`/homework-submit/${id}`)}
+          bgcolor="darkblue"
+          color="white"
+          padding="5px 15px"
         >
-          <Button
-            onClick={() => navigate(`/homework-submit/${id}`)}
-            bgcolor="darkblue"
-            color="white"
-            padding="5px 15px"
-          >
-            제출하러 가기
-          </Button>
-        </div>
-      </Styled.Modal>
+          제출하러 가기
+        </Button>
+      </div>
     </>
   );
 }
