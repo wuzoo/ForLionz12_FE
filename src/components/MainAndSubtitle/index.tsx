42b46d0b8fc66cb66cef4971d7a9484e5d7adcd2
wuzoo <wndyd9911@gmail.com>
2024-02-21 23:18:26 +0900

    style: fontSize 반응형으로 수정. 2/3 사이즈

--- src/components/MainAndSubtitle/index.tsx
+++ src/components/MainAndSubtitle/index.tsx
@@ -7,7 +7,15 @@ function MainAndSubtitle({ main, sub, fontsizes, colors, gap }: ITitle) {
   const [mainsize, subsize] = fontsizes;
   return (
     <div>
-      <Styled.Title>
+      <Styled.Title
+        css={css`
+          @media screen and (max-width: 768px) {
+            span {
+              font-size: ${(+mainsize * 2) / 3}px;
+            }
+          }
+        `}
+      >
         <Typo weight="700" color={colors[0]} fontSize={mainsize}>
           {main}
         </Typo>
@@ -15,9 +23,20 @@ function MainAndSubtitle({ main, sub, fontsizes, colors, gap }: ITitle) {
       <div
         css={css`
           height: ${gap}px;
+          @media screen and (max-width: 768px) {
+            height: 0;
+          }
         `}
       ></div>
-      <Styled.Content>
+      <Styled.Content
+        css={css`
+          @media screen and (max-width: 768px) {
+            span {
+              font-size: ${(+subsize * 2) / 3}px;
+            }
+          }
+        `}
+      >
         <Typo color={colors[1]} fontSize={subsize} weight="500">
           {sub}
         </Typo>
