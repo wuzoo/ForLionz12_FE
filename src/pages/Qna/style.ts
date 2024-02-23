7fa8d6d8f526a6c2b98529e19026cee1e368ae18
wuzoo <wndyd9911@gmail.com>
2024-02-23 21:42:24 +0900

    feat: query string으로 data filter하여 pagination 구현

--- src/pages/Qna/style.ts
+++ src/pages/Qna/style.ts
@@ -3,6 +3,8 @@ import styled from "@emotion/styled";
 export const Wrapper = styled.div`
   width: 60%;
   min-height: 150vh;
+  display: flex;
+  flex-direction: column;
 `;
 
 export const Title = styled.h1``;
@@ -18,4 +20,11 @@ export const ItemsContainer = styled.div`
 
 export const TagsAndBtnWrapper = styled.div`
   ${(props) => props.theme.flexRow("space-between", "center")}
+`;
+
+export const PageBtnWrapper = styled.div`
+  display: flex;
+  place-self: center;
+  padding-top: 60px;
+  gap: 8px;
 `;
