83d24c362051619e342248749d22a34b18cf8ada
wuzoo <wndyd9911@gmail.com>
2024-02-26 21:57:59 +0900

    refactor: 반응형 디자인 수정

--- src/pages/Home/components/category/Category.tsx
+++ src/pages/Home/components/category/Category.tsx
@@ -3,6 +3,7 @@ import * as Styled from "./style";
 import { ColorAndImgofItem, Destination } from "../../constant/constant";
 import { useNavigate } from "react-router-dom";
 import Typo from "../../../../components/Typo/Typo";
+import { theme } from "../../../../styles/theme/theme";
 
 function Category() {
   const colors = Array.from(Object.values(ColorAndImgofItem));
@@ -11,23 +12,31 @@ function Category() {
   const navigate = useNavigate();
 
   return (
-    <Styled.Container>
-      {colors.map((item, index) => (
-        <Styled.ItemWrapper key={item[0]}>
-          <Styled.Item
-            onClick={() => navigate(Destination[nav[index]])}
-            css={css`
-              background-color: ${item[0]};
-            `}
-          >
-            <Styled.Img src={item[1]} />
-          </Styled.Item>
-          <Styled.Text>
-            <Typo color="darkgray">{nav[index]}</Typo>
-          </Styled.Text>
-        </Styled.ItemWrapper>
-      ))}
-    </Styled.Container>
+    <div
+      css={css`
+        ${theme.flexRow("center", "center")}
+        width: 100%;
+        padding-top: 50px;
+      `}
+    >
+      <Styled.Container>
+        {colors.map((item, index) => (
+          <Styled.ItemWrapper key={item[0]}>
+            <Styled.Item
+              onClick={() => navigate(Destination[nav[index]])}
+              css={css`
+                background-color: ${item[0]};
+              `}
+            >
+              <Styled.Img src={item[1]} />
+            </Styled.Item>
+            <Styled.Text>
+              <Typo color="darkgray">{nav[index]}</Typo>
+            </Styled.Text>
+          </Styled.ItemWrapper>
+        ))}
+      </Styled.Container>
+    </div>
   );
 }
 
