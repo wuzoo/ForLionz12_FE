969947ee42a240e9d5fd499ec5a8c32130122ceb
wuzoo <wndyd9911@gmail.com>
2024-03-03 17:02:21 +0900

    feat: url이 null | undefined일 시 디폴트 유저 svg 컴포넌트로 대체

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -1,7 +1,21 @@
 import { User } from "./style";
 import { IProfile } from "./types";
-import user from "../../assets/icons/user/defaultUser.svg";
+import DefaultUser from "../../assets/icons/user/defaultUser.svg?react";
+import { useContext } from "react";
+import { ThemeContext } from "../../context/IsDark/IsDark";
 
 export default function Profile(props: IProfile) {
-  return <User src={props.url || user} {...props} />;
+  const { isDark } = useContext(ThemeContext);
+
+  if (props.url === null || props.url === undefined) {
+    return (
+      <DefaultUser
+        stroke={isDark ? "white" : "black"}
+        width={props.size || "50"}
+        height={props.size || "50"}
+      />
+    );
+  }
+
+  return <User src={props.url} {...props} />;
 }
