953019c05951a54e127f599b13a1a85d52cc6786
wuzoo <wndyd9911@gmail.com>
2024-03-03 20:39:13 +0900

    feat: text-overflow: ellipsis를 사용한 말줄임 Ellipsis Text 컴포넌트 생성

---
+++ src/components/Ellipsis/types.ts
@@ -0,0 +1,11 @@
+import { ReactNode } from "react";
+
+export interface IText {
+  children: ReactNode;
+  color: string;
+  lineHeight?: number;
+  lineClamp?: number;
+  padding?: string;
+  overflow?: string;
+  display?: string;
+}
