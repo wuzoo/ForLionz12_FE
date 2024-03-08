8e159a49c3cdda97d8b64071293ad0ed849d22a3
wuzoo <wndyd9911@gmail.com>
2024-03-08 20:45:53 +0900

    refactor: img -> svg 컴포넌트로 교체 + Theme 적용

--- src/components/Slider/components/Arrow.tsx
+++ src/components/Slider/components/Arrow.tsx
@@ -1,15 +1,24 @@
-import left from "../assets/left.svg";
-import right from "../assets/right.svg";
+import Left from "../assets/left.svg?react";
+import Right from "../assets/right.svg?react";
 import * as Styled from "../components/style";
 
 interface IArrow {
   onClick: (e: React.MouseEvent<HTMLImageElement>) => void;
+  isDark: boolean;
 }
 
-export function Next({ onClick }: IArrow) {
-  return <Styled.Right onClick={onClick} src={right} />;
+export function Next({ onClick, isDark }: IArrow) {
+  return (
+    <Styled.Right onClick={onClick}>
+      <Right fill={isDark ? "white" : "black"} />
+    </Styled.Right>
+  );
 }
 
-export function Prev({ onClick }: IArrow) {
-  return <Styled.Left onClick={onClick} src={left} />;
+export function Prev({ onClick, isDark }: IArrow) {
+  return (
+    <Styled.Left onClick={onClick}>
+      <Left fill={isDark ? "white" : "black"} />
+    </Styled.Left>
+  );
 }
