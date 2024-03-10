136118bf0ae0442d2ed6b397843e8bb5abfa3e42
wuzoo <wndyd9911@gmail.com>
2024-03-10 23:46:21 +0900

    style: boxShadow opacity 수정

--- src/components/ListItem/NoticeIndex/variants.ts
+++ src/components/ListItem/NoticeIndex/variants.ts
@@ -1,7 +1,7 @@
 export const variants = {
   hover: {
     scale: 1.01,
-    boxShadow: "0px 3px 10px rgba(178, 204, 229, 0.5)",
+    boxShadow: "0px 3px 10px rgba(178, 204, 229, 0.3)",
     transition: {
       duration: 0.2,
     },
