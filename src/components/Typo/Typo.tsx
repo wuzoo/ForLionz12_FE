e91399a065942bcb41436002a278167fcab3e3df
wuzoo <wndyd9911@gmail.com>
2024-03-14 19:49:18 +0900

    feat: window font 호환 x -> font 수정 후 기존 weight prop에 맞는 font-family 적용

--- src/components/Typo/Typo.tsx
+++ src/components/Typo/Typo.tsx
@@ -6,17 +6,36 @@ import { css } from "@emotion/react";
 import { theme } from "../../styles/theme/theme";
 
 const StyledTypo = styled.span<ITypo>`
-  font-weight: ${(props) => props.weight || "500"};
   font-size: ${(props) => props.fontSize || "18"}px;
+  font-family: ${(props) => props.weight};
 
   transition: all 0.2s ease-in-out;
 `;
 
+function handleFont(weight: string) {
+  switch (weight) {
+    case "700": {
+      return "Pretendard-bold";
+    }
+    case "600": {
+      return "Pretendard-semibold";
+    }
+    case "500": {
+      return "Pretendard-medium";
+    }
+    case "400": {
+      return "Pretendard-regular";
+    }
+  }
+}
+
 export default function Typo(props: ITypo) {
   const { isDark } = useContext(ThemeContext);
+
   return (
     <StyledTypo
       {...props}
+      weight={handleFont(props.weight || "500")}
       css={css`
         color: ${props.color
           ? theme.color[props.color]
