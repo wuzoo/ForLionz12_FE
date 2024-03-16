bb75f836fb99dac11aba81fd6e676cd2e89f552c
wuzoo <wndyd9911@gmail.com>
2024-03-16 14:25:20 +0900

    chore: import문 주석, lazy 코드 사용

--- src/App.tsx
+++ src/App.tsx
@@ -4,22 +4,22 @@ import { Global, ThemeProvider } from "@emotion/react";
 import { theme } from "./styles/theme/theme";
 import Layout from "./layout/Layout/Layout";
 import Login from "./pages/Login/Login";
-import Home from "./pages/Home/Home";
-import HwList from "./pages/Hw-list/Hw-list";
-import HwSubmit from "./pages/Hw-submit/Hw-submit";
-import Contact from "./pages/Contact/Contact";
-import Notification from "./pages/Notification/Notification";
-import Profile from "./pages/Profile/Profile";
-import Qna from "./pages/Qna/Qna";
-// const Home = React.lazy(() => import("./pages/Home/Home"));
-// const HwList = React.lazy(() => import("./pages/Hw-list/Hw-list"));
-// const HwSubmit = React.lazy(() => import("./pages/Hw-submit/Hw-submit"));
-// const Contact = React.lazy(() => import("./pages/Contact/Contact"));
-// const Notification = React.lazy(
-//   () => import("./pages/Notification/Notification")
-// );
-// const Profile = React.lazy(() => import("./pages/Profile/Profile"));
-// const Qna = React.lazy(() => import("./pages/Qna/Qna"));
+// import Home from "./pages/Home/Home";
+// import HwList from "./pages/Hw-list/Hw-list";
+// import HwSubmit from "./pages/Hw-submit/Hw-submit";
+// import Contact from "./pages/Contact/Contact";
+// import Notification from "./pages/Notification/Notification";
+// import Profile from "./pages/Profile/Profile";
+// import Qna from "./pages/Qna/Qna";
+const Home = React.lazy(() => import("./pages/Home/Home"));
+const HwList = React.lazy(() => import("./pages/Hw-list/Hw-list"));
+const HwSubmit = React.lazy(() => import("./pages/Hw-submit/Hw-submit"));
+const Contact = React.lazy(() => import("./pages/Contact/Contact"));
+const Notification = React.lazy(
+  () => import("./pages/Notification/Notification")
+);
+const Profile = React.lazy(() => import("./pages/Profile/Profile"));
+const Qna = React.lazy(() => import("./pages/Qna/Qna"));
 import { LoginInfoProvider } from "./context/LoginUser/User";
 import UploadHW from "./pages/Admin/UploadAssignment";
 import UploadNotice from "./pages/Admin/UploadNotification";
